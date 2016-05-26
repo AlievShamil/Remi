ad8328d13a88120347fa142e60e49815e83aef2e
devHazhi <dmhazhi@gmail.com>
2016-05-26 13:39:06 +0300

    Создание простой поректной структуры

---
+++ app/src/main/java/com/devcom/remi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.devcom.remi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HAMANN on 26.05.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
